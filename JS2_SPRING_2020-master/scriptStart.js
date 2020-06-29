5e28375c8a0b57e0e8106e0297c7fac8251a14f7
carrollc35187 <54286058+carrollc35187@users.noreply.github.com>
2020-06-29 13:26:01 -0400

    uploaded files for week 1 assignment

---
+++ JS2_SPRING_2020-master/scriptStart.js
@@ -0,0 +1,28 @@
+// declare a value for the humanScore variable to be the number 0
+let humanScore = 0;
+
+// declare a variable named 'computer score' using camelCase 
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+
+// declare a variable that cannot be change (it's constant) that is named generateTarget
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    const humanDifference = Math.abs(targetGuess - humanGuess)
+    const computerDifference = Math.abs(targetGuess - computerGuess)
+    return humanDifference <= computerDifference;
+}
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    }
+}
+
+const advanceRound = () => currentRoundNumber++;
